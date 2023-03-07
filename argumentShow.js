208e7b6b00dcfae97d4d1761da99320eff00cb4b
Cyrille <cyrillechateau@icloud.com>
2023-03-07 23:27:40 +0100

    add js code

---
+++ argumentShow.js
@@ -0,0 +1,10 @@
+
+
+let mySentence = "hello world oui oui non non";
+let myArray = mySentence.split(" ");
+console.log(myArray);
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
+
